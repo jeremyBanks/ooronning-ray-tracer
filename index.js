ab2dbd64f5217e2ca72f7d24c425cc7be9b851aa
Christopher Ronning <christopher.a.ronning@gmail.com>
2017-10-29 16:09:07 -0400

    refactor chapter 1 solution

--- index.js
+++ index.js
@@ -1,67 +1,56 @@
 'use strict';
 
-// class CanvasRenderer {
-//   constructor(canvas) {
-//     this.canvas = canvas;
-//     this.canvas.width = 200;
-//     this.canvas.height = 100;
-//     this.context = this.canvas.getContext('2d');
-//   }
-//
-//   generateGradientImage() {
-//     for (let j = this.canvas.height - 1; j >= 0; j--) {
-//       for (let i = 0; i < this.canvas.width; i++) {
-//         const r = i / this.canvas.width;
-//         const g = j / this.canvas.height;
-//         const b = 0.2;
-//
-//         const ir = 255.99 * r;
-//         const ig = 255.99 * g;
-//         const ib = 255.99 * b;
-//       }
-//     }
-//   }
-//   render() {
-//
-//   }
-// }
+class CanvasPainter {
+  constructor(canvas, width, height) {
+    this.canvas = canvas;
+    this.canvas.width = width ? width : 500;
+    this.canvas.height = height ? height : 500;
+    this.context = this.canvas.getContext('2d');
+  }
+  
+  render() {
+    const imageData = this.context.getImageData(0, 0, this.canvas.width, this.canvas.height);
+    let offset = 0;
+
+    for (let j = this.canvas.height - 1; j >= 0; j--) {
+      for (let i = 0; i < this.canvas.width; i++) {
+
+        const r = i / this.canvas.width;
+        const g = j / this.canvas.height;
+        const b = 0.2;
+        const a = '0xFF'; // 255
+
+        const ir = 255.99 * r;
+        const ig = 255.99 * g;
+        const ib = 255.99 * b;
+        const ia = a;
+
+        imageData.data[offset] = ir;
+        imageData.data[offset + 1] = ig;
+        imageData.data[offset + 2] = ib;
+        imageData.data[offset + 3] = ia;
+
+        offset += 4;
+      }
+    }
+
+    this.context.putImageData(imageData, 0, 0);
+    document.getElementById('target').appendChild(this.canvas);
+  }
+}
+
+const generateImage = canvas => {
+  const img = new Image();
+  img.src = canvas.toDataURL('image/png');
+  document.getElementById('target').appendChild(img);
+};
+
 const main = () => {
   const canvas = document.createElement('canvas');
-  canvas.width = 200;
-  canvas.height = 100;
-  const context = canvas.getContext('2d');
-  const imageData = context.getImageData(0, 0, canvas.width, canvas.height);
+  const painter = new CanvasPainter(canvas, 200, 100);
 
-  const img = new Image();
-
-  for (let j = canvas.height; j >= 0; j--) {
-    for (let i = 0; i < canvas.width; i++) {
-
-      const offset = (j * canvas.width + i) * 4;
-      const r = i / canvas.width;
-      const g = j / canvas.height;
-      const b = 0.2;
-      const a = '0xFF';
-
-      const ir = 255.99 * r;
-      const ig = 255.99 * g;
-      const ib = 255.99 * b;
-      const ia = a;
-
-      imageData.data[offset + 0] = ir;
-      imageData.data[offset + 1] = ig;
-      imageData.data[offset + 2] = ib;
-      imageData.data[offset + 3] = ia;
-    }
-  }
-
-  console.log(imageData)
-
-  context.putImageData(imageData, 0, 0);
-  document.getElementById('target').appendChild(canvas);
-
-  img.src = canvas.toDataURL("image/png");
-  document.getElementById('target').appendChild(img);
+  painter.render();
+  generateImage(canvas);
 };
 
 main();
